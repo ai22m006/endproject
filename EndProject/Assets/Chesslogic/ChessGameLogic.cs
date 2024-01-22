bc11a59fdabd4c422aeff5afe54cd67f0d26f735
ai22m006 <113135472+ai22m006@users.noreply.github.com>
2024-01-22 20:17:27 +0100

    updated chess logic

--- EndProject/Assets/Chesslogic/ChessGameLogic.cs
+++ EndProject/Assets/Chesslogic/ChessGameLogic.cs
@@ -8,13 +8,14 @@ public class ChessGameLogic : MonoBehaviour
     GameObject clickedPiece;
     GameObject WhiteClock;
     GameObject BlackClock;
-    
+    string turn = "White";
+
     // Start is called before the first frame update
     void Start()
     {
         WhiteClock = GameObject.FindWithTag("Wclock");
         BlackClock = GameObject.FindWithTag("Bclock");
-        WhiteClock.GetComponent<Clock>().clockactive = true;
+        WhiteClock.GetComponent<Clock>().clockactive = false;
         BlackClock.GetComponent<Clock>().clockactive = false;
     }
 
@@ -31,28 +32,56 @@ public class ChessGameLogic : MonoBehaviour
                 string color = clickedObject.tag;
                 string type = clickedObject.name;
                 UnityEngine.Debug.Log("Clicked on " + color + " " + type);
-
+                
                 if (color == "White" || color == "Black") {
-                    clickedPiece = clickedObject;
-                    UnityEngine.Debug.Log("Clicked on " + clickedPiece.tag + " " + clickedPiece.name);
+                    if (clickedPiece == null && color == turn)
+                    {
+                        clickedPiece = clickedObject;
+                        UnityEngine.Debug.Log("Clicked on " + clickedPiece.tag + " " + clickedPiece.name);
+                    }
+
+                    if (clickedPiece != null && clickedPiece != clickedObject && clickedPiece.tag != clickedObject.tag)
+                    {
+                        clickedPiece.transform.position = clickedObject.transform.position;
+                        Destroy(clickedObject);
+                        if (clickedPiece.tag == "White")
+                        {
+                            WhiteClock.GetComponent<Clock>().clockactive = false;
+                            BlackClock.GetComponent<Clock>().clockactive = true;
+                            turn = "Black";
+                        }
+                        else
+                        {
+                            WhiteClock.GetComponent<Clock>().clockactive = true;
+                            BlackClock.GetComponent<Clock>().clockactive = false;
+                            turn = "White";
+                        }
+                        clickedPiece = null;
+                    }
+
                 }
 
                 if (type == "Chess Board" && clickedPiece != null)
                 {
                     // Move the clicked piece to the position where the mouse was clicked
-                    Vector3 newPosition = hit.point;
-                    newPosition.y = clickedPiece.transform.position.y; // Keep the same y-coordinate
+                    Vector3 newPosition = hit.point; // x y z
+                    //GameObject ChessBoard = GameObject.FindWithTag("ChessBoard");
+                    //newPosition.y = clickedPiece.transform.position.y; // Keep the same y-coordinate
+                    
+                    
                     clickedPiece.transform.position = newPosition;
                     UnityEngine.Debug.Log("Move " + clickedPiece.tag + " " + clickedPiece.name + " to new position: " + newPosition);
                     if (clickedPiece.tag == "White")
                     {
                         WhiteClock.GetComponent<Clock>().clockactive = false;
                         BlackClock.GetComponent<Clock>().clockactive = true;
+                        turn = "Black";
                     }
                     else
                     {
                         WhiteClock.GetComponent<Clock>().clockactive = true;
                         BlackClock.GetComponent<Clock>().clockactive = false;
+                        turn = "White";
                     }
                     clickedPiece = null;
                 }
